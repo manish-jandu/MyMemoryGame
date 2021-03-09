0a50507cce7189913b38a3838876cf635aaa6562
Manish <manishjandu1@gmail.com>
2021-03-09 10:22:38 +0530

    can change no of cards with enum

--- app/src/main/java/com/example/mymemory/MainActivity.kt
+++ app/src/main/java/com/example/mymemory/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.mymemory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.mymemory.models.BoardSize
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var boardSize:BoardSize = BoardSize.HARD
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        
+        rvBoard.adapter = MemoryBoardAdapter(this,boardSize)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this,boardSize.getWidth())
     }
 }
